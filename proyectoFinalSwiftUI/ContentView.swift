300cd9dc9cd30399ae8beed4266bebd71f75e0bc
Juan Gamino <gamino_juan03@hotmail.com>
2020-04-09 17:18:24 -0500

    Initial Commit

---
+++ proyectoFinalSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  proyectoFinalSwiftUI
+//
+//  Created by Juan on 09/04/20.
+//  Copyright © 2020 usuario. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
